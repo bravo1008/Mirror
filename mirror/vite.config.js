558268aafbeeb1f32361f18937e8233c65c25ac4
JingYi Sun <2307836152@qq.com>
2026-01-14 11:06:36 +0800

    部署

--- mirror/vite.config.js
+++ mirror/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://mirror-lcd5.onrender.com", // 后端地址
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
